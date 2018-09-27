97345384e51f40154107f3014fd02f17a871617a
jonathan <jonathan.wailam.lee@gmail.com>
2018-09-27 14:05:34 -0300

    setup to install experimental code

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+
+from glob import glob
+from os.path import splitext, basename
+from setuptools import find_packages, setup
+
+
+setup(
+    name='alignclf',
+    version='0.1.0',
+    description='Experiment setup for alignment algorithm classification',
+    author='Wai Lam Jonathan Lee',
+    author_email='walee@uc.cl',
+    install_requires=['numpy', 'pandas'],
+    test_requires=['pytest'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_module=[splitext(basename(path))[0] for path in glob('src/*.py')]
+)
