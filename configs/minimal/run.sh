2ef7e32ba86cb0090b97026891c1c65b4d596b82
jonathan <jonathan.wailam.lee@gmail.com>
2018-09-27 18:10:46 -0300

    Minimal runner configs

--- configs/minimal/run.sh
+++ configs/minimal/run.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-PYTHON=python
-RUN=./src/run.py
-CONFIG=./configs/minimal/configs.json
-LOGGINGCONFIGS=./src/logging.json
+cd prom-nightly
 
+PYTHON=python
+RUN=../src/run.py
+CONFIG=../configs/minimal/configs.json
+LOGGINGCONFIGS=../src/logging.json
 
 # variables containing spaces need to be surrounded with double quotes so that
 # they are passed as one single variable!
