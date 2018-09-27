4cca9ce10850da89f04c19b76e4ac7ff1d9a605a
jonathan <jonathan.wailam.lee@gmail.com>
2018-09-27 14:05:47 -0300

    Minimal working setup

---
+++ configs/minimal/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+PYTHON=python
+RUN=./src/run.py
+CONFIG=./configs/minimal/configs.json
+LOGGINGCONFIGS=./src/logging.json
+
+
+# variables containing spaces need to be surrounded with double quotes so that
+# they are passed as one single variable!
+$PYTHON $RUN -c "${CONFIG}" -l "${LOGGINGCONFIGS}"
+
+echo "All done!"
