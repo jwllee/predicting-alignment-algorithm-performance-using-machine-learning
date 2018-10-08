8a5d55c08c91c40ed82eb9602b5c01897724d04f
jonathan <jonathan.wailam.lee@gmail.com>
2018-10-08 11:19:51 -0300

    Added monolithic replay configurations

---
+++ configs/test-monolithic/run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# activate the virtual environment
+VENV="alignclf-venv/bin/activate"
+. ${VENV}
+
+which python
+
+cd prom-nightly
+
+PYTHON=python
+RUN=../src/run.py
+CONFIG=../configs/test-monolithic/configs.json
+LOGGINGCONFIGS=../src/logging.json
+TIME_FILE="../times.txt"
+
+N=1
+
+# variables containing spaces need to be surrounded with double quotes so that
+# they are passed as one single variable!
+for i in $(seq 1 $N)
+do
+    echo "Iteration $i"
+    ../scripts/time -a -o ${TIME_FILE} -f "Command: %C\n[mm:ss.ms]: %E\n" $PYTHON $RUN -c "${CONFIG}" -l "${LOGGINGCONFIGS}"
+    sleep 5
+done
+
+echo "All done!"
