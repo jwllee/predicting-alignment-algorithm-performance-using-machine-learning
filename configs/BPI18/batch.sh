71b99d40b5901b1a211b9a5ba27b416cc6e267ae
jonathan <jonathan.wailam.lee@gmail.com>
2018-11-22 16:00:21 -0300

    Update configs

--- configs/BPI18/batch.sh
+++ configs/BPI18/batch.sh
@@ -2,12 +2,11 @@
 
 # THIS FILE WAS GENERATED AUTOMATICALLY.
 
-# sh "configs/BPI18/mono-inc+/run.sh"
+sh "configs/BPI18/mono-inc3/run.sh"
+sh "configs/BPI18/inc3-sese_25-score-strict/run.sh"
+sh "configs/BPI18/mono-inc+/run.sh"
 sh "configs/BPI18/mono-astar/run.sh"
-# sh "configs/BPI18/mono-inc0/run.sh"
-# sh "configs/BPI18/mono-inc3/run.sh"
-# sh "configs/BPI18/inc+-sese_25-score-strict/run.sh"
+sh "configs/BPI18/mono-inc0/run.sh"
+sh "configs/BPI18/inc+-sese_25-score-strict/run.sh"
 sh "configs/BPI18/astar-sese_25-score-strict/run.sh"
-# sh "configs/BPI18/inc0-sese_25-score-strict/run.sh"
-# sh "configs/BPI18/inc3-sese_25-score-strict/run.sh"
-# sh "configs/BPI18/inc-sese_25-score-strict/run.sh"
+sh "configs/BPI18/inc0-sese_25-score-strict/run.sh"
