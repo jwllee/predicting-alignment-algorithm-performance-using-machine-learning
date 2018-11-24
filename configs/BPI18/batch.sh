62cec9ad60a6b54507438084baa52b7a77b5fa7c
jonathan <jonathan.wailam.lee@gmail.com>
2018-11-24 15:40:03 -0300

    update BPI configs

--- configs/BPI18/batch.sh
+++ configs/BPI18/batch.sh
@@ -2,8 +2,8 @@
 
 # THIS FILE WAS GENERATED AUTOMATICALLY.
 
-sh "configs/BPI18/mono-inc3/run.sh"
 sh "configs/BPI18/inc3-sese_25-score-strict/run.sh"
+sh "configs/BPI18/mono-inc3/run.sh"
 sh "configs/BPI18/mono-inc+/run.sh"
 sh "configs/BPI18/mono-astar/run.sh"
 sh "configs/BPI18/mono-inc0/run.sh"
