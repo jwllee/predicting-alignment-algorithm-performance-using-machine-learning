de67a35eedeb5fe9becb4bcf04408c2a6dc39d18
jonathan <jonathan.wailam.lee@gmail.com>
2019-01-19 18:40:39 -0300

    WIP

--- src/alignclf/net_feature_extract.py
+++ src/alignclf/net_feature_extract.py
@@ -3,6 +3,9 @@
 
 import podspy
 import podspy.petrinet
+import numpy as np
+import functools as fct
+import collections as cols
 
 
 # net features
@@ -22,6 +25,10 @@ DUP_TRAN_MEAN_IN_DEG = 'dup_transition_mean_in_degree'
 DUP_TRAN_STD_IN_DEG = 'dup_transition_std_in_degree'
 DUP_TRAN_MEAN_OUT_DEG = 'dup_transition_mean_out_degree'
 DUP_TRAN_STD_OUT_DEG = 'dup_transition_std_out_degree'
+PLACE_MEAN_IN_DEG = 'place_mean_in_deg'
+PLACE_STD_IN_DEG = 'place_std_in_deg'
+PLACE_MEAN_OUT_DEG = 'place_mean_out_deg'
+PLACE_STD_OUT_DEG = 'place_std_out_deg'
 N_PLACE = 'n_place'
 N_ARC = 'n_arc'
 N_AND_SPLIT = 'n_and_split'
@@ -49,30 +56,54 @@ def get_n_tran(pn):
     return len(pn.transitions)
 
 
+def get_dup_tran_dict(pn):
+    assert isinstance(pn, podspy.petrinet.Petrinet)
+    tran_dict = cols.defaultdict(list)
+    for t in pn.transitions:
+        if t.is_invisible:
+            continue
+        tran_dict[t.label].append(t)
+    to_remove = [t_label for t_label in tran_dict.keys() if len(tran_dict[t_label]) == 1]
+    for t_label in to_remove:
+        del tran_dict[t_label]
+    return dict(tran_dict)
+
+
+def get_dup_tran_list(pn):
+    assert isinstance(pn, podspy.petrinet.Petrinet)
+    dup_tran = get_dup_tran_dict(pn)
+    return fct.reduce(lambda _all, tran_list: _all + tran_list, dup_tran, [])
+
+
+def get_uniq_tran_list(pn):
+    assert isinstance(pn, podspy.petrinet.Petrinet)
+    tran_dict = cols.defaultdict(list)
+    for t in pn.transitions:
+        if t.is_invisible:
+            continue
+        tran_dict[t.label].append(t)
+    uniq_tran = [tran_dict[t_label] for t_label in tran_dict.keys() if len(tran_dict[t_label]) == 1]
+    return fct.reduce(lambda _all, tran_set: _all + list(tran_set), uniq_tran, [])
+
+
+def get_inv_tran_list(pn):
+    assert isinstance(pn, podspy.petrinet.Petrinet)
+    return list(filter(lambda t: t.is_invisible, pn.transitions))
+
+
 def get_n_inv_tran(pn):
     assert isinstance(pn, podspy.petrinet.Petrinet)
-    inv_tran = filter(lambda t: t.is_invisible(), pn.transitions)
-    return len(list(inv_tran))
+    return len(get_inv_tran_list(pn))
 
 
 def get_n_dup_tran(pn):
     assert isinstance(pn, podspy.petrinet.Petrinet)
-    tran_dict = {}
-    for t in pn.transitions:
-        cnt = tran_dict.get(t.label, default=0)
-        tran_dict[t.label] = cnt + 1
-    dup_tran = filter(lambda t_label: tran_dict[t_label] > 1, tran_dict.keys())
-    return len(list(dup_tran))
+    return len(list(get_dup_tran_dict(pn).keys()))
 
 
 def get_n_uniq_tran(pn):
     assert isinstance(pn, podspy.petrinet.Petrinet)
-    tran_dict = {}
-    for t in pn.transitions:
-        cnt = tran_dict.get(t.label, default=0)
-        tran_dict[t.label] = cnt + 1
-    uniq_tran = filter(lambda t_label: tran_dict[t_label] == 1, tran_dict.keys())
-    return len(list(uniq_tran))
+    return len(get_uniq_tran_list(pn))
 
 
 def get_n_place(pn):
@@ -114,5 +145,222 @@ def get_n_xor_split(pn):
     xor_split = filter(lambda p: adj_list[p] > 1, adj_list.keys())
     return len(list(xor_split))
 
+
+def get_inv_tran_in_deg_list(pn):
+    assert isinstance(pn, podspy.petrinet.Petrinet)
+    inv_tran = get_inv_tran_list(pn)
+    in_deg_list = []
+    for t in inv_tran:
+        t_edges = pn.get_directed_edges(target=t)
+        in_deg_list.append(len(t_edges))
+    return in_deg_list
+
+
+def get_inv_tran_out_deg_list(pn):
+    assert isinstance(pn, podspy.petrinet.Petrinet)
+    inv_tran = get_inv_tran_list(pn)
+    out_deg_list = []
+    for t in inv_tran:
+        t_edges = pn.get_directed_edges(src=t)
+        out_deg_list.append(len(t_edges))
+    return out_deg_list
+
+
+def get_inv_tran_mean_in_deg(pn):
+    in_deg_list = get_inv_tran_in_deg_list(pn)
+    return np.mean(in_deg_list) if in_deg_list else 0.
+
+
+def get_inv_tran_std_in_deg(pn):
+    in_deg_list = get_inv_tran_in_deg_list(pn)
+    return np.std(in_deg_list) if in_deg_list else 0.
+
+
+def get_inv_tran_mean_out_deg(pn):
+    out_deg_list = get_inv_tran_out_deg_list(pn)
+    return np.mean(out_deg_list) if out_deg_list else 0.
+
+
+def get_inv_tran_std_out_deg(pn):
+    out_deg_list = get_inv_tran_out_deg_list(pn)
+    return np.std(out_deg_list) if out_deg_list else 0.
+
+
+def get_uniq_tran_in_deg_list(pn):
+    assert isinstance(pn, podspy.petrinet.Petrinet)
+    uniq_tran = get_uniq_tran_list(pn)
+    in_deg_list = []
+    for t in uniq_tran:
+        t_edges = pn.get_directed_edges(target=t)
+        in_deg_list.append(len(t_edges))
+    return in_deg_list
+
+
+def get_uniq_tran_out_deg_list(pn):
+    assert isinstance(pn, podspy.petrinet.Petrinet)
+    uniq_tran = get_uniq_tran_list(pn)
+    out_deg_list = []
+    for t in uniq_tran:
+        t_edges = pn.get_directed_edges(src=t)
+        out_deg_list.append(len(t_edges))
+    return out_deg_list
+
+
+def get_uniq_tran_mean_in_deg(pn):
+    in_deg_list = get_uniq_tran_in_deg_list(pn)
+    return np.mean(in_deg_list) if in_deg_list else 0.
+
+
+def get_uniq_tran_std_in_deg(pn):
+    in_deg_list = get_uniq_tran_in_deg_list(pn)
+    return np.std(in_deg_list) if in_deg_list else 0.
+
+
+def get_uniq_tran_mean_out_deg(pn):
+    return np.mean(get_uniq_tran_out_deg_list(pn))
+
+
+def get_uniq_tran_std_out_deg(pn):
+    return np.mean(get_uniq_tran_out_deg_list(pn))
+
+
+def get_dup_tran_in_deg_list(pn):
+    assert isinstance(pn, podspy.petrinet.Petrinet)
+    dup_tran = get_dup_tran_list(pn)
+    in_deg_list = []
+    for t in dup_tran:
+        t_edges = pn.get_directed_edges(target=t)
+        in_deg_list.append(len(t_edges))
+    return in_deg_list
+
+
+def get_dup_tran_out_deg_list(pn):
+    assert isinstance(pn, podspy.petrinet.Petrinet)
+    dup_tran = get_dup_tran_list(pn)
+    out_deg_list = []
+    for t in dup_tran:
+        t_edges = pn.get_directed_edges(src=t)
+        out_deg_list.append(len(t_edges))
+    return out_deg_list
+
+
+def get_dup_tran_mean_in_deg(pn):
+    return np.mean(get_dup_tran_in_deg_list(pn))
+
+
+def get_dup_tran_std_in_deg(pn):
+    return np.std(get_dup_tran_in_deg_list(pn))
+
+
+def get_dup_tran_mean_out_deg(pn):
+    return np.mean(get_uniq_tran_out_deg_list(pn))
+
+
+def get_dup_tran_std_out_deg(pn):
+    return np.std(get_dup_tran_out_deg_list(pn))
+
+
+def get_place_in_deg_list(pn):
+    assert isinstance(pn, podspy.petrinet.Petrinet)
+    in_deg_list = []
+    for p in pn.places:
+        p_edges = pn.get_directed_edges(target=p)
+        in_deg_list.append(len(p_edges))
+    return in_deg_list
+
+
+def get_place_out_deg_list(pn):
+    assert isinstance(pn, podspy.petrinet.Petrinet)
+    out_deg_list = []
+    for p in pn.places:
+        p_edges = pn.get_directed_edges(src=p)
+        out_deg_list.append(len(p_edges))
+    return out_deg_list
+
+
+def pn_to_undirected(pn):
+    # return as adjacency list
+    adj = cols.defaultdict(list)
+
+    for node in pn.get_nodes():
+        for edge in pn.get_directed_edges(src=node):
+            adj[node].append(edge.target)
+            adj[edge.target].append(node)
+
+    return dict(adj)
+
+
+def get_biconnected_component_list(pn):
+    assert isinstance(pn, podspy.petrinet.Petrinet)
+    components = list()
+
+    G = pn_to_undirected(pn)
+
+    # print('Graph: ')
+    #
+    # for node in G.keys():
+    #     for v in G[node]:
+    #         print('{}-{}'.format(node.label, v.label), end=' ')
+    #     print()
+    #
+    # print()
+
+    visited = set()
+
+    for start in G.keys():
+        if start in visited:
+            continue
+
+        # print('{} as root'.format(start.label))
+
+        discovery = {start:0}
+        low = {start:0}
+        root_children = 0
+        visited.add(start)
+        edge_stack = []
+        stack = list()
+        stack.append((start, start, iter(G[start])))
+
+        while stack:
+            grandparent, parent, children = stack[-1]
+            try:
+                child = next(children)
+                if grandparent == child:
+                    # print('Skip {}-{}'.format(parent.label, child.label))
+                    continue
+                # print('Visit {}-{}'.format(parent.label, child.label))
+                if child in visited:
+                    if discovery[child] <= discovery[parent]:   # back edge
+                        low[parent] = min(low[parent], discovery[child])
+                        edge_stack.append((parent, child))
+                else:
+                    low[child] = discovery[child] = len(discovery)
+                    visited.add(child)
+                    stack.append((parent, child, iter(G[child])))
+                    edge_stack.append((parent, child))
+            except StopIteration:
+                stack.pop()
+                # print('Stopped at {} {}'.format(grandparent.label, parent.label))
+                if len(stack) > 1:
+                    if low[parent] >= discovery[grandparent]:
+                        # print('Bi: {}-{}'.format(grandparent.label, parent.label))
+                        # grandparent is an articulation point
+                        ind = edge_stack.index((grandparent, parent))
+                        components.append(edge_stack[ind:])
+                        edge_stack = edge_stack[:ind]
+                    low[grandparent] = min(low[parent], low[grandparent])
+                elif stack: # grandparent is root
+                    root_children += 1
+                    ind = edge_stack.index((grandparent, parent))
+                    components.append(edge_stack[ind:])
+                    # no need to empty stack until ind because the next component
+                    # will start from top of stack, i.e., after ind
+
+    return components
+
+
+def get_n_biconnected_component(pn):
+    components = get_biconnected_component_list(pn)
+    return len(components)
 
 
---
+++ tests/alignclf/test_net_feature_extract.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python
+
+
+import podspy
+from podspy.petrinet import *
+import pytest
+
+
+from alignclf import net_feature_extract
+
+
+class TestExtractNetFeature:
+    @pytest.fixture(
+        scope='function'
+    )
+    def net_t4_p3_a8(self):
+        net = PetrinetFactory.new_petrinet('net0')
+
+        t_a = net.add_transition('a')
+        t_b = net.add_transition('b')
+        t_c = net.add_transition('c')
+        t_d = net.add_transition('d')
+
+        p_1 = net.add_place('p1')
+        p_2 = net.add_place('p2')
+        p_3 = net.add_place('p3')
+
+        net.add_arc(p_1, t_a),
+        net.add_arc(p_1, t_b),
+        net.add_arc(p_2, t_c),
+        net.add_arc(p_2, t_d),
+        net.add_arc(t_a, p_2),
+        net.add_arc(t_b, p_2),
+        net.add_arc(t_c, p_3),
+        net.add_arc(t_d, p_3)
+
+        return net
+
+    def test_get_n_tran(self, net_t4_p3_a8):
+        expected = len(net_t4_p3_a8.transitions)
+        assert net_feature_extract.get_n_tran(net_t4_p3_a8) == expected
+
+    def test_get_n_inv_tran(self):
+        net = PetrinetFactory.new_petrinet('net0')
+        net.add_transition('a', is_invisible=True)
+        net.add_transition('b')
+        expected = 1
+        assert net_feature_extract.get_n_inv_tran(net) == expected
+
+    def test_get_n_dup_tran_should_not_include_invisible_transitions(self):
+        net = PetrinetFactory.new_petrinet('net0')
+        net.add_transition('inv', is_invisible=True)
+        net.add_transition('inv', is_invisible=True)
+        expected = 0
+        assert net_feature_extract.get_n_dup_tran(net) == expected
+
+    def test_get_n_dup_tran_non_zero_case(self):
+        net = PetrinetFactory.new_petrinet('net0')
+        net.add_transition('a')
+        net.add_transition('a')
+        net.add_transition('b')
+        expected = 1
+        assert net_feature_extract.get_n_dup_tran(net) == expected
+
+    def test_get_n_uniq_tran_should_not_include_invisible(self):
+        net = PetrinetFactory.new_petrinet('net0')
+        net.add_transition('inv', is_invisible=True)
+        expected = 0
+        assert net_feature_extract.get_n_uniq_tran(net) == expected
+
+    def test_get_n_uniq_tran_non_zero_case(self):
+        net = PetrinetFactory.new_petrinet('net0')
+        net.add_transition('a')
+        net.add_transition('b')
+        net.add_transition('c')
+        net.add_transition('a')
+        expected = 2
+        assert net_feature_extract.get_n_uniq_tran(net) == expected
+
+    def test_get_n_place(self, net_t4_p3_a8):
+        expected = 3
+        assert net_feature_extract.get_n_place(net_t4_p3_a8) == expected
+
+    def test_get_n_xor_split_zero_case(self):
+        net = PetrinetFactory.new_petrinet('net0')
+        net.add_transition('a')
+        expected = 0
+        assert net_feature_extract.get_n_xor_split(net) == expected
+
+    def test_get_n_xor_split_non_zero_case(self, net_t4_p3_a8):
+        expected = 2
+        assert net_feature_extract.get_n_xor_split(net_t4_p3_a8) == expected
+
+    def test_get_n_and_split_zero_case(self, net_t4_p3_a8):
+        expected = 0
+        assert net_feature_extract.get_n_and_split(net_t4_p3_a8) == expected
+
+    def test_get_n_and_split_non_zero_case(self):
+        net = PetrinetFactory.new_petrinet('net0')
+        tran_labels = ['a', 'b', 'c', 'd', 'e']
+        trans = dict()
+        places = dict()
+        for label in tran_labels:
+            trans[label] = net.add_transition(label)
+        for i in range(8):
+            places[i + 1] = net.add_place('p{}'.format(i + 1))
+
+        net.add_arc(places[1], trans['a'])
+        net.add_arc(places[2], trans['b'])
+        net.add_arc(places[3], trans['c'])
+        net.add_arc(places[4], trans['d'])
+        net.add_arc(places[5], trans['e'])
+        net.add_arc(places[6], trans['e'])
+        net.add_arc(places[7], trans['e'])
+
+        net.add_arc(trans['a'], places[2])
+        net.add_arc(trans['a'], places[3])
+        net.add_arc(trans['a'], places[4])
+        net.add_arc(trans['b'], places[5])
+        net.add_arc(trans['c'], places[6])
+        net.add_arc(trans['d'], places[7])
+        net.add_arc(trans['e'], places[8])
+
+        expected = 1
+        assert net_feature_extract.get_n_and_split(net) == expected
+
+    def test_get_inv_tran_mean_in_deg_zero_case(self, net_t4_p3_a8):
+        expected = 0.
+        assert net_feature_extract.get_inv_tran_mean_in_deg(net_t4_p3_a8) == expected
+
+    def test_get_inv_tran_std_in_deg_zero_case(self, net_t4_p3_a8):
+        expected = 0.
+        assert net_feature_extract.get_inv_tran_std_in_deg(net_t4_p3_a8) == expected
+
+    def test_get_n_biconnected_component_without_bridge(self):
+        # make net
+        net_label = 'net0'
+        net = PetrinetFactory.new_petrinet(net_label)
+
+        t_a = net.add_transition('a')
+        t_b = net.add_transition('b')
+        t_c = net.add_transition('c')
+        t_d = net.add_transition('d')
+
+        p_1 = net.add_place('p1')
+        p_2 = net.add_place('p2')
+        p_3 = net.add_place('p3')
+
+        net.add_arc(p_1, t_a)
+        net.add_arc(p_1, t_b)
+        net.add_arc(p_2, t_c)
+        net.add_arc(p_2, t_d)
+        net.add_arc(t_a, p_2)
+        net.add_arc(t_b, p_2)
+        net.add_arc(t_c, p_3)
+        net.add_arc(t_d, p_3)
+
+        assert net_feature_extract.get_n_biconnected_component(net) == 2
+
+    def test_get_n_biconnected_component_with_bridge(self):
+        """The undirected graph of the petrinet has two bridges at (p2, c) and (c, p3).
+        These bridges count as minimal biconnected components.
+
+        """
+        net_label = 'net0'
+        net = PetrinetFactory.new_petrinet(net_label)
+
+        t_a = net.add_transition('a')
+        t_b = net.add_transition('b')
+        t_c = net.add_transition('c')
+        t_d = net.add_transition('d')
+        t_e = net.add_transition('e')
+
+        p_1 = net.add_place('p1')
+        p_2 = net.add_place('p2')
+        p_3 = net.add_place('p3')
+        p_4 = net.add_place('p4')
+
+        net.add_arc(p_1, t_a)
+        net.add_arc(p_1, t_b)
+        net.add_arc(p_2, t_c)
+        net.add_arc(p_3, t_d)
+        net.add_arc(p_3, t_e)
+
+        net.add_arc(t_a, p_2)
+        net.add_arc(t_b, p_2)
+        net.add_arc(t_c, p_3)
+        net.add_arc(t_d, p_4)
+        net.add_arc(t_e, p_4)
+
+        assert net_feature_extract.get_n_biconnected_component(net) == 4
+
