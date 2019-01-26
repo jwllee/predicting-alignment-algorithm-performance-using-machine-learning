8c6ef0f14f62819e5b2b2f127aa95398b8ee2f69
jonathan <jonathan.wailam.lee@gmail.com>
2019-01-26 18:46:09 -0300

    iterate through particular directories

--- src/run_create_clf_data.py
+++ src/run_create_clf_data.py
@@ -12,95 +12,111 @@ from alignclf import create_clf_data
 
 
 if __name__ == '__main__':
-    result_dir = os.path.join('.', 'results-agg', 'clst-IS2017-2-repacked')
+    result_dnames = [
+        'clst-2018-12-generic_50-inc0-net1',
+        'clst-2018-12-generic_50-inc0-net2',
+        # 'clst-2018-12-generic_50-inc0-net3',
+        # 'clst-2018-12-generic_50-inc0-net4',
+        # 'clst-2018-12-generic_50-inc0-net5',
+        'clst-2018-12-sese_25-inc0-net1',
+        'clst-2018-12-sese_25-inc0-net2',
+        # 'clst-2018-12-sese_25-inc0-net3',
+        # 'clst-2018-12-sese_25-inc0-net4',
+        # 'clst-2018-12-sese_25-inc0-net5'
+    ]
 
     # find out the subset of logs
-    model_log_sets = []
-    dir_map = dict()
+    for result_dname in result_dnames:
+        result_dir = os.path.join('.', 'results-agg', result_dname)
 
-    for d in os.listdir(result_dir):
-        dirpath = os.path.join(result_dir, d)
+        print('Processing {}'.format(result_dname))
 
-        if not os.path.isdir(dirpath):
-            continue
+        model_log_sets = []
+        dir_map = dict()
 
-        model_log_set = set()
+        for d in os.listdir(result_dir):
+            dirpath = os.path.join(result_dir, d)
 
-        for replay_d in os.listdir(dirpath):
-
-            replay_dirpath = os.path.join(dirpath, replay_d)
-
-            if not os.path.isdir(replay_dirpath):
+            if not os.path.isdir(dirpath):
                 continue
 
-            configs_fp = os.path.join(replay_dirpath, 'configs.json')
-            with open(configs_fp) as f:
-                configs_dict = json.load(f)
+            model_log_set = set()
 
-            log = configs_dict['log']
-            model = configs_dict['model']
-            if 'recomposeStrategy' in configs_dict:
-                algo_type = 'recomp' + '-' + configs_dict['algorithmType']
-            else:
-                algo_type = configs_dict['algorithmType']
+            for replay_d in os.listdir(dirpath):
 
-            if model not in dir_map:
-                dir_map[model] = cols.defaultdict(list)
+                replay_dirpath = os.path.join(dirpath, replay_d)
 
-            dir_map[model][log].append((algo_type, replay_dirpath))
-            model_log_set.add((model, log))
+                if not os.path.isdir(replay_dirpath):
+                    continue
 
-        model_log_sets.append(model_log_set)
+                configs_fp = os.path.join(replay_dirpath, 'configs.json')
+                with open(configs_fp) as f:
+                    configs_dict = json.load(f)
 
-    model_logs = list(fct.reduce(lambda s1, s2: s1.intersection(s2), model_log_sets))
-    model_log_dict = cols.defaultdict(list)
-
-    for model, log in model_logs:
-        model_log_dict[model].append(log)
-
-    # print('Model and logs: {}'.format(model_logs))
-    # print('Model log set: {}'.format(model_log_sets))
-
-    clf_df_list = list()
-
-    for model, logs in model_log_dict.items():
-
-        if not logs:
-            continue
-
-        for log in logs:
-
-            result_df_dict = dict()
-            for algo_type, dirpath in dir_map[model][log]:
-
-                is_mono = 'recomp' not in algo_type
-
-                # print('algo_type: {}'.format(algo_type))
-
-                if is_mono:
-                    result_fp = os.path.join(dirpath, 'trace-stats-enriched.csv')
-                    result_df = pd.read_csv(result_fp)
-                    result_df[create_clf_data.RESULT_DIR] = dirpath
-                    result_df = create_clf_data.process_df(result_df)
+                log = configs_dict['log']
+                model = configs_dict['model']
+                if 'recomposeStrategy' in configs_dict:
+                    algo_type = 'recomp' + '-' + configs_dict['algorithmType']
                 else:
-                    result_fp = os.path.join(dirpath, 'trace-stats.csv')
-                    result_df = pd.read_csv(result_fp)
-                    result_df[create_clf_data.RESULT_DIR] = dirpath
-                    result_df = create_clf_data.process_recomposing_df(result_df)
+                    algo_type = configs_dict['algorithmType']
 
-                result_df_dict[algo_type] = result_df
+                if model not in dir_map:
+                    dir_map[model] = cols.defaultdict(list)
 
-            clf_df = create_clf_data.to_clf_df(result_df_dict)
+                dir_map[model][log].append((algo_type, replay_dirpath))
+                model_log_set.add((model, log))
 
-            columns = list(clf_df.columns)
-            clf_df['model'] = model
-            clf_df['log'] = log
-            columns = [('model', ''), ('log', '')] + columns
-            clf_df = clf_df[columns]
+            model_log_sets.append(model_log_set)
 
-            clf_df_list.append(clf_df)
+        model_logs = list(fct.reduce(lambda s1, s2: s1.intersection(s2), model_log_sets))
+        model_log_dict = cols.defaultdict(list)
 
-    clf_df = pd.concat(clf_df_list, axis=0)
-    out_fp = os.path.join(result_dir, 'perf-results.csv')
-    clf_df.to_csv(out_fp, index=False)
+        for model, log in model_logs:
+            model_log_dict[model].append(log)
+
+        # print('Model and logs: {}'.format(model_logs))
+        # print('Model log set: {}'.format(model_log_sets))
+
+        clf_df_list = list()
+
+        for model, logs in model_log_dict.items():
+
+            if not logs:
+                continue
+
+            for log in logs:
+
+                result_df_dict = dict()
+                for algo_type, dirpath in dir_map[model][log]:
+
+                    is_mono = 'recomp' not in algo_type
+
+                    # print('algo_type: {}'.format(algo_type))
+
+                    if is_mono:
+                        result_fp = os.path.join(dirpath, 'trace-stats-enriched.csv')
+                        result_df = pd.read_csv(result_fp)
+                        result_df[create_clf_data.RESULT_DIR] = dirpath
+                        result_df = create_clf_data.process_df(result_df)
+                    else:
+                        result_fp = os.path.join(dirpath, 'trace-stats.csv')
+                        result_df = pd.read_csv(result_fp)
+                        result_df[create_clf_data.RESULT_DIR] = dirpath
+                        result_df = create_clf_data.process_recomposing_df(result_df)
+
+                    result_df_dict[algo_type] = result_df
+
+                clf_df = create_clf_data.to_clf_df(result_df_dict)
+
+                columns = list(clf_df.columns)
+                clf_df['model'] = model
+                clf_df['log'] = log
+                columns = [('model', ''), ('log', '')] + columns
+                clf_df = clf_df[columns]
+
+                clf_df_list.append(clf_df)
+
+        clf_df = pd.concat(clf_df_list, axis=0)
+        out_fp = os.path.join(result_dir, '{}-predictive-output.csv'.format(result_dname))
+        clf_df.to_csv(out_fp, index=False)
 
