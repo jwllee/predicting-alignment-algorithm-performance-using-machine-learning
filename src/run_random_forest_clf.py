d4cad68a43884fb263d0d96e0da204ef1e2200f3
jonathan <jonathan.wailam.lee@gmail.com>
2019-01-31 16:48:52 -0300

    update

--- src/run_random_forest_clf.py
+++ src/run_random_forest_clf.py
@@ -53,7 +53,7 @@ def get_uniq_count(l):
     return dict(zip(uniq, count))
 
 
-def train_forest(X, y):
+def train_forest(X, y, file):
     n_procs = mp.cpu_count() - 1
     class_weight = 'balanced'
 
@@ -154,9 +154,9 @@ if __name__ == '__main__':
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
 
-    best_clf, oob_err_df = train_forest(X_train, y_train)
+    best_clf, oob_err_df = train_forest(X_train, y_train, file)
 
-    perform_test(X_test, y_test, best_clf)
+    perform_test(X_test, y_test, best_clf, file)
 
     # save trained model
     with open(forest_fp, 'wb') as f:
